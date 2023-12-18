1740f18bc85524738b2942f4cad6a122d70c78bd
Devesh Kataria <Devesh.Kataria@dpwapps.com>
2023-12-18 10:44:00 +0000

    removing delete lock

--- projects/cdpz/data-storage/main.tf
+++ projects/cdpz/data-storage/main.tf
@@ -2,11 +2,11 @@ data "azurerm_resource_group" "resgrp" {
   name      = join("-", ["cdpz", var.environment, "data-storage-rg"])
 }
 
-resource "azurerm_management_lock" "delete-lock" {
-  name       = "resource-group-deletion-lock"
-  scope      = data.azurerm_resource_group.resgrp.id
-  lock_level = "CanNotDelete"
-}
+# resource "azurerm_management_lock" "delete-lock" {
+#   name       = "resource-group-deletion-lock"
+#   scope      = data.azurerm_resource_group.resgrp.id
+#   lock_level = "CanNotDelete"
+# }
 
 data "azurerm_key_vault" "kv" {
   name                  = var.sandbox_prefix == "" ? join("-", ["cdpz", var.environment, "data-kv"]) : join("-", ["cdpz", var.environment, var.sandbox_prefix, "data-kv"])
