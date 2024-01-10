98a99836812627f6722598c863434decba97bec7
shoeb <shoebece@gmail.com>
2024-01-10 17:55:33 +0400

    updated

--- projects/cdmz/networking/main.tf
+++ projects/cdmz/networking/main.tf
@@ -210,13 +210,13 @@ resource "azurerm_route_table" "art" {
       next_hop_in_ip_address  = var.uae-cpperimeter81-prod
     },
     {
-      name                    = "Route_Constanta_Fivetran"
+      name                    = "Route_LCIT_Fivetran"
       address_prefix          = "172.19.240.120/32"
       next_hop_type           = "VirtualAppliance"
       next_hop_in_ip_address  = var.vpn_firewall_ip_address
     },
     {
-      name                    = "Route_Constanta_Fivetran"
+      name                    = "Route_Pusan_Fivetran"
       address_prefix          = "10.2.2.105/32"
       next_hop_type           = "VirtualAppliance"
       next_hop_in_ip_address  = var.vpn_firewall_ip_address
