d9f45106060389951429efab3db7f77a89ae41af
shoeb <shoebece@gmail.com>
2024-02-13 14:00:04 +0400

    modified the route

--- projects/cdmz/networking/main.tf
+++ projects/cdmz/networking/main.tf
@@ -249,13 +249,13 @@ resource "azurerm_route_table" "art" {
       next_hop_in_ip_address  = var.firewall_ip_address
     },
     {
-      name                    = "Route_Antwerp_Fivetran"
+      name                    = "Route_SCO_Fivetran"
       address_prefix          = "10.91.5.58/32"
       next_hop_type           = "VirtualAppliance"
       next_hop_in_ip_address  = var.vpn_firewall_ip_address
     }
   ]
-
+  
   tags = merge(var.resource_tags_common, var.resource_tags_spec)
 }
 
