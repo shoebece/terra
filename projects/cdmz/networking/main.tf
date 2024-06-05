bb589593d8601fe63803f239a436c0f637a1ce71
shoeb <shoebece@gmail.com>
2024-06-05 07:22:30 +0400

    made modifications

--- projects/cdmz/networking/main.tf
+++ projects/cdmz/networking/main.tf
@@ -1417,10 +1417,10 @@ resource "azurerm_private_endpoint" "AzureSQL_mpowered_endpoint_pep" {
 
   custom_network_interface_name = "cdmz-mgmt-fivetran-mpowered-nic"
 
-  private_dns_zone_group {
-    name = "add_to_azure_private_dns_sql"
-    private_dns_zone_ids = [ data.azurerm_private_dns_zone.pdnsz_sql.id ]
-  }
+  # private_dns_zone_group {
+  #   name = "add_to_azure_private_dns_sql"
+  #   private_dns_zone_ids = [ data.azurerm_private_dns_zone.pdnsz_sql.id ]
+  # }
 
   private_service_connection {
     name                           = "cdmz-mgmt-fivetran-pdnsz_sql-psc"
@@ -1429,12 +1429,12 @@ resource "azurerm_private_endpoint" "AzureSQL_mpowered_endpoint_pep" {
     is_manual_connection           = false
   }
 
-  ip_configuration {
-    name               = "cdmz-mgmt-fivetran-mpowered-ipc"
-    private_ip_address = var.mpowered_fv_ip_address
-    subresource_name   = "sqlServer"
-    member_name        = "sqlServer"
-  }
+  # ip_configuration {
+  #   name               = "cdmz-mgmt-fivetran-mpowered-ipc"
+  #   private_ip_address = var.mpowered_fv_ip_address
+  #   subresource_name   = "sqlServer"
+  #   member_name        = "sqlServer"
+  # }
 
   tags = merge(
     var.resource_tags_spec
